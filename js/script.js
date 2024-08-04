9439b97eff1d085ddc9570ae1e574a6861f0a849
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2024-08-04 21:33:43 +0100

    finish styling + some js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const colorPicker = document.getElementById('color-picker')
+const colorSchemeModeMenu = document.getElementById('color-scheme-menu')
+const submitColorScheme = document.getElementById('color-scheme-submit')
+
+let colorHexCode = colorPicker.value.substring(1)
+let colorSchemeMode = colorSchemeModeMenu.value
+
+console.log(colorHexCode)
+console.log(colorSchemeMode)
+
+colorPicker.addEventListener('input', function(e) {
+  colorHexCode = e.target.value.substring(1)
+})
+
+colorSchemeModeMenu.addEventListener('input', function(e) {
+  colorSchemeMode = e.target.value
+})
+
+submitColorScheme.addEventListener('click', function(e) {
+  e.preventDefault()
+  let displayColorScheme = document.getElementById('display-color-scheme')
+  let displayColorSchemeHtml = ''
+
+  // get colour scheme 
+  fetch(`https://www.thecolorapi.com/scheme?hex=${colorHexCode}&mode=${colorSchemeMode}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    data.colors.forEach(color => {
+      console.log(color.hex)
+    })
+
+  })
+
+
+
+  displayColorScheme.innerHTML = displayColorSchemeHtml 
+})
+
